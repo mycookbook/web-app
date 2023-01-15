f5a703eb46c5297cd29a9ee6b6cc5259cc514634
Kingsley Solomon <johnkingzy@gmail.com>
2023-01-15 03:10:22 +0100

    fixes error with build

--- app.vue
+++ app.vue
@@ -4,9 +4,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script lang="ts">
-// import 'semantic-ui-css/semantic.min'
-// import './assets/js/jquery.min.js'
-// import './assets/js/semantic.min.js'
-// import './assets/js/script.js'
-</script>
