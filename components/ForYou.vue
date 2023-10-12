f42e4040d1ce58d0e78b7e0183b6f170719a3a39
fokosun <okosunuzflorence@gmail.com>
2023-10-12 17:58:40 -0400

    import feed page

---
+++ components/ForYou.vue
@@ -0,0 +1,28 @@
+<template>
+    <div style="margin-top:25px;">
+        <TrendingThisWeek />
+        <br />
+        <div class="ui divider"></div>
+        <br />
+        <Feedback />
+        <br />
+        <div class="ui divider"></div>
+        <TakingOrders />
+        <br /><br />
+        <TrendingNow />
+        <br />
+        <div class="ui divider"></div>
+        <br /><br />
+        <PeopleWithSimilarInterests />
+        <div>
+            <a href="/">back to top</a>
+        </div>
+    </div>
+</template>
+    
+<script lang="ts">
+
+export default {
+    name: "ForYou"
+};
+</script>
